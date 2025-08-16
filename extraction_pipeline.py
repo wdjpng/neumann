ff2f04b86dcf1e02d5004554d06445d6d4d2db0a
Lukas Münzel <muenzel.lukas@gmail.com>
2025-08-16 12:15:21 +0200

    Stopped saving individual pages & fix type error

--- extraction_pipeline.py
+++ extraction_pipeline.py
@@ -5,6 +5,7 @@ from PIL import Image
 import pymupdf
 import io
 import os
+import shutil
 
 
 async def process_folder(folder_path : Path, output_path = Path("public/outputs_gpt-5") ):
@@ -12,7 +13,9 @@ async def process_folder(folder_path : Path, output_path = Path("public/outputs_
     for file in os.listdir(folder_path):
         if file.endswith(".pdf"):
             pdf = pymupdf.open(folder_path/file)
+            shutil.rmtree(output_path/file[:-4], ignore_errors=True)
             (output_path/file[:-4]/"chunks").mkdir(parents=True, exist_ok=True)
+            shutil.copy2(folder_path/file, output_path/file[:-4]/"letter.pdf")
             processed_files.append(asyncio.create_task(process_pdf(pdf, output_path/file[:-4])))
 
     await asyncio.gather(*processed_files)
@@ -24,16 +27,6 @@ async def process_pdf(pdf : pymupdf.Document, output_path : Path):
     # Use ~4.17x scale for 300 DPI (300/72 = 4.167)
     matrix = pymupdf.Matrix(4.167, 4.167)
     images = [Image.open(io.BytesIO(page.get_pixmap(matrix=matrix).tobytes("png"))) for page in pdf]
-   
-    # TODO undo
-    # Save all images in output_path and reload them
-    image_paths = []
-    (output_path / "pages").mkdir(parents=True, exist_ok=True)
-    for i, img in enumerate(images):
-        img_path = output_path / "pages" / f"page_{i+1}.png"
-        img.save(img_path)
-        image_paths.append(img_path)
-    images = [Image.open(p) for p in image_paths]
     
     async def _with_index(i, task):
         return i, await task
@@ -46,10 +39,9 @@ async def process_pdf(pdf : pymupdf.Document, output_path : Path):
         print(f"Page {page_idx} has {len(coords)} chunks")
         for chunk_idx, img in enumerate(chunk_extractor.save_chunks(images[page_idx], coords, output_path, page_idx)):
             entries.append((page_idx, chunk_idx, asyncio.create_task(text_extractor.transcribe_chunk(img))))
-
-    texts_reasoning_pairs = await asyncio.gather(*[t for _, _, t in sorted(entries)])
-    print("Extracted text for all chunks")
-    text_extractor.log_chunks(texts_reasoning_pairs, output_path/"chunks")
+    
+    texts_reasoning_pairs = await asyncio.gather(*[t for _, _, t in entries])
+    text_extractor.log_chunks([(page_idx, chunk_idx, *task.result()) for (page_idx, chunk_idx, task) in entries], output_path/"chunks")
 
     html_de, reasoning = await text_extractor.unite_html([text for text, _ in texts_reasoning_pairs], images[0])
 
